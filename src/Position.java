287de0398d946dd2415bea607de17faf4bb11269
Dylan Rakotomahefa <dylan@unistra.fr>
2024-11-21 00:11:12 +0100

    Tile dev.ed un ENCORE plus

---
+++ src/Position.java
@@ -0,0 +1,8 @@
+public class Position {
+    public int _x;
+    public int _y;
+
+    public String printPosition() {
+        System.out.println("x = " + this._x + " , " + "y = " + this._y);
+    }
+}
---
+++ src/Tiles.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+
+public abstract class Tiles {
+    public Position _position;
+    public ArrayList<Tiles> _listVoisins;
+}
