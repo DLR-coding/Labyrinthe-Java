c26af1e0632c4c93b5f797c66bab28e98ea889f1
Dylan Rakotomahefa <dylan@unistra.fr>
2024-12-01 20:38:35 +0100

    umltestV3 , La testV2 est meilleur. La V3 n'est pas encore normalisée

--- src/Board.java
+++ src/Board.java
@@ -1,32 +1,61 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Board {
-    private ArrayList<ArrayList<Tiles>> tiles;
-    private Tiles extraTile;
+    private ArrayList<ArrayList<Tile>> _tiles;
+    private Tile _extraTile;
 
     public Board() {
-        this.tiles = new ArrayList<>();
+        this._tiles = new ArrayList<>();
         for (int i = 0; i < 7; i++) {
-            ArrayList<Tiles> row = new ArrayList<>();
+            ArrayList<Tile> row = new ArrayList<>();
             for (int j = 0; j < 7; j++) {
                 row.add(null); // or initialize with a Tiles object if needed
             }
-            tiles.add(row);
+            _tiles.add(row);
         }
     }
 
-    public Tiles getTile(Position position) {
-        return tiles.get(position.getX()).get(position.getY());
+    private void initializeBoard() {
+        // Initialiser le plateau avec des tuiles et des objectifs
+        TileFactory tileFactory = new TileFactory();
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                Tile tile = tileFactory.createTileStraight(Orientation.NORTH, null);
+                this.setTile(new Position(i, j), tile);
+            }
+        }
+        // addGoalsToBoard();
     }
 
-    public void setTile(Position position, Tiles tile) {
-        tiles.get(position.getX()).set(position.getY(), tile);
-        System.out.println("Tuile placée en (" + position.getX() + ", " + position.getY() + ").");
+
+    public Tile getTile(Position position) {
+        return _tiles.get(position.getRow()).get(position.getCol());
     }
 
-    public void moveRowOrColumn(Move move) {
-        // Implémentation du déplacement de ligne ou colonne
+    public void setTile(Position position, Tile tile) {
+        _tiles.get(position.getRow()).set(position.getCol(), tile);
     }
 
-    // Autres méthodes pour générer et placer les tuiles, déplacer les lignes/colonnes, etc.
+
+
+    public void printBoard(List<Pawn> pawns) {
+        System.out.println("Plateau de jeu:");
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                Tile tile = _tiles.get(j).get(i);
+                if (tile == null) {
+                    System.out.print("[  x , x , x , x ,]");
+                } else {
+                System.out.print("[ " + tile.getDisplayPaths() +  "]" );
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println("Positions des pions:");
+        for (Pawn pawn : pawns) {
+            System.out.println("Pion à la position: (" + pawn.getPosition().getRow() + ", " + pawn.getPosition().getCol() + ")");
+        }
+    }
 }
--- src/Pawn.java
+++ src/Pawn.java
@@ -2,69 +2,65 @@ public class Pawn {
     private Position _position;
 
     public Pawn(Position position) {
-        this.position = position;
+        this._position = position;
     }
 
-    public boolean move(Move move, Board board) {
-        if (isMoveValid(move, board)) {
-            switch (move.getDirection()) {
-                case 0: // Haut
-                    position.setY(position.getY() - 1);
-                    break;
-                case 1: // Droite
-                    position.setX(position.getX() + 1);
-                    break;
-                case 2: // Bas
-                    position.setY(position.getY() + 1);
-                    break;
-                case 3: // Gauche
-                    position.setX(position.getX() - 1);
-                    break;
-            }
-            return true;
-        }
-        return false;
-    }
-
-    public boolean isMoveValid(Move move, Board board) {
-        Position newPosition = new Position(position.getX(), position.getY());
-        switch (move.getDirection()) {
-            case 0: // HautS
-                newPosition.setY(newPosition.getY() - 1);
+    public boolean move(Direction direction, Board board) {
+        Position newPosition = new Position(this._position.getRow(), this._position.getCol());
+        switch (direction) {
+            case UP:
+                newPosition.setCol(newPosition.getCol() - 1);
                 break;
-            case 1: // Droite
-                newPosition.setX(newPosition.getX() + 1);
+            case RIGHT:
+                newPosition.setRow(newPosition.getRow() + 1);
                 break;
-            case 2: // Bas
-                newPosition.setY(newPosition.getY() + 1);
+            case DOWN:
+                newPosition.setCol(newPosition.getCol() + 1);
                 break;
-            case 3: // Gauche
-                newPosition.setX(newPosition.getX() - 1);
+            case LEFT:
+                newPosition.setRow(newPosition.getRow() - 1);
                 break;
         }
 
-        if (newPosition.getX() < 0 || newPosition.getX() >= 7 || newPosition.getY() < 0 || newPosition.getY() >= 7) {
+        if (newPosition.getRow() < 0 || newPosition.getRow() >= 7 || newPosition.getCol() < 0 || newPosition.getCol() >= 7) {
             return false; // Hors des limites du plateau
         }
 
-        Tiles currentTile = board.getTile(position);
-        Tiles newTile = board.getTile(newPosition);
+        Tile currentTile = board.getTile(this._position);
+        Tile newTile = board.getTile(newPosition);
 
         if (currentTile == null || newTile == null) {
             return false; // Pas de tuile à la position actuelle ou nouvelle
         }
 
-        return currentTile.isPathOpen(move.getDirection()) && newTile.isPathOpen((move.getDirection() + 2) % 4);
+        if (currentTile.isPathOpen(direction) && newTile.isPathOpen(getOppositeDirection(direction))) {
+            this._position = newPosition;
+            return true;
+        }
+
+        return false;
     }
 
+    private Direction getOppositeDirection(Direction direction) {
+        switch (direction) {
+            case UP:
+                return Direction.DOWN;
+            case RIGHT:
+                return Direction.LEFT;
+            case DOWN:
+                return Direction.UP;
+            case LEFT:
+                return Direction.RIGHT;
+            default:
+                throw new IllegalArgumentException("Invalid direction: " + direction);
+        }
+    }
 
-
-    // Getters and setters
     public Position getPosition() {
-        return position;
+        return _position;
     }
 
-    public void setPosition(Position position) {
-        this.position = position;
+    public void set_position(Position position) {
+        this._position = position;
     }
 }
