96b6bc6375a1bfe7468b7e5353f36495fc509128
vhatt <victor.hatt@unistra.fr>
2024-12-22 20:01:21 +0100

    update de l'UML et du rapport2.md (+changements mineurs dans GameUI.java)

--- src/GameUI.java
+++ src/GameUI.java
@@ -4,109 +4,109 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 public class GameUI extends JFrame {
-    private final JPanel game;
-    private JPanel board;
-    private JLabel[][] tiles;
+    private final JPanel _game;
+    private JPanel _board;
+    private JLabel[][] _tiles;
 
     private JLabel _extraTile;
 
     public GameUI(){
         this.setSize(1200, 1200);
-        game = new JPanel(new GridBagLayout());
-        board = new JPanel(new GridLayout(7,7));
-        this.add(game);
+        _game = new JPanel(new GridBagLayout());
+        _board = new JPanel(new GridLayout(7,7));
+        this.add(_game);
         GridBagConstraints c = new GridBagConstraints();
         c.fill = GridBagConstraints.NONE;
         c.weightx = 0.5;
         c.weighty = 0.5;
         c.gridy = 1;
         c.gridx = 1;
-        game.add(board, c);
-        tiles = new JLabel[7][7];
+        _game.add(_board, c);
+        _tiles = new JLabel[7][7];
         for (int i = 0; i < 7; i++){
             for (int j = 0; j < 7; j++){
-                tiles[i][j] = new JLabel();
-                tiles[i][j].setSize(80, 80);
-                board.add(tiles[i][j]);
+                _tiles[i][j] = new JLabel();
+                _tiles[i][j].setSize(80, 80);
+                _board.add(_tiles[i][j]);
             }
         }
         this._extraTile = new JLabel();
         _extraTile.setSize(80,80);
 
 
-        JPanel buttonsTop = new JPanel(new GridLayout(1,3));
-        buttonsTop.add(new JButton("topBtn0"));
-        buttonsTop.add(new JButton("topBtn1"));
-        buttonsTop.add(new JButton("topBtn2"));
-        c.weightx= 1;
-        c.weighty= 1;
-        c.gridy = 0;
-        game.add(buttonsTop, c);
 
-        JPanel buttonsBtm = new JPanel(new GridLayout(1,3));
-        buttonsBtm.add(new JButton("btmBtn0"));
-        buttonsBtm.add(new JButton("btmBtn1"));
-        buttonsBtm.add(new JButton("btmBtn2"));
-        c.weightx= 0.1;
-        c.weighty= 0.1;
-        c.gridy = 2;
-        game.add(buttonsBtm, c);
+        JPanel dpad = new JPanel(new GridLayout(3, 3, 5, 5));
+            JButton up = new JButton("Up");
+            JButton down = new JButton("Down");
+            JButton left = new JButton("Left");
+            JButton right = new JButton("Right");
+            dpad.add(new JLabel()); // Spacer
+            dpad.add(up);
+            dpad.add(new JLabel()); // Spacer
+            dpad.add(left);
+            dpad.add(new JLabel()); // Center spacer
+            dpad.add(right);
+            dpad.add(new JLabel()); // Spacer
+            dpad.add(down);
+            dpad.add(new JLabel()); // Spacer
+            c.gridx = 0;
+            c.gridy = 1;
+            c.gridwidth = 1; // Single column
+            c.gridheight = 1; // One row
+            c.weightx = 0.5; // Allow some horizontal space
+            c.weighty = 0.2; // Less vertical space
+            c.fill = GridBagConstraints.NONE;
+            c.anchor = GridBagConstraints.WEST;
+            _game.add(dpad, c);
 
-        JPanel buttonsLeft = new JPanel(new GridLayout(3,1));
-        buttonsLeft.add(new JButton("leftBtn0"));
-        buttonsLeft.add(new JButton("leftBtn1"));
-        buttonsLeft.add(new JButton("leftBtn2"));
-        c.weightx= 0.1;
-        c.weighty= 0.1;
-        c.gridx = 0;
-        c.gridy = 1;
-        game.add(buttonsLeft, c);
-
-        JPanel buttonsRight = new JPanel(new GridLayout(3,1));
-        buttonsRight.add(new JButton("rightBtn0"));
-        buttonsRight.add(new JButton("rightBtn1"));
-        buttonsRight.add(new JButton("rightBtn2"));
-        c.weightx= 0.1;
-        c.weighty= 0.1;
-        c.gridx = 2;
-        game.add(buttonsRight, c);
+            // Button setup
+            JButton actionButton = new JButton("Action");
+            c.gridx = 1;
+            c.gridy = 1;
+            c.gridwidth = 1;
+            c.gridheight = 1;
+            c.weightx = 0.5;
+            c.weighty = 0.2;
+            c.fill = GridBagConstraints.NONE;
+            c.anchor = GridBagConstraints.EAST;
+            _game.add(actionButton, c);
 
         this.setVisible(true);
     }
 
-    public static void setImage(JLabel tile, BufferedImage image){
+    private static void setImage(JLabel tile, BufferedImage image){
         tile.setIcon(new ImageIcon(image));
     }
 
-    public static void rotateTileClockwise(JLabel tile){
+    private static void rotateTileClockwise(JLabel tile){
         setImage(tile, ImageHelper.rotateClockwise((BufferedImage)tile.getIcon()));
     }
 
-    public static void rotateTileTrig(JLabel tile){
+    /* public static void rotateTileTrig(JLabel tile){
         setImage(tile, ImageHelper.rotateCounterClockwise((BufferedImage)tile.getIcon()));
-    }
+    } */ // no current use for this
 
     private void moveRow(int row, Direction direction) throws Exception {
         if(direction == Direction.RIGHT){
-            ImageIcon tempIcon = (ImageIcon) this.tiles[row][6].getIcon();
+            ImageIcon tempIcon = (ImageIcon) this._tiles[row][6].getIcon();
             for (int i =6; i>=0; i--){
                 if(i==0) {
-                    tiles[row][i].setIcon(this._extraTile.getIcon());
+                    _tiles[row][i].setIcon(this._extraTile.getIcon());
                 }
                 else{
-                    tiles[row][i].setIcon(tiles[row][i-1].getIcon());
+                    _tiles[row][i].setIcon(_tiles[row][i-1].getIcon());
                 }
                 this._extraTile.setIcon(tempIcon);
             }
         }
         else if(direction == Direction.LEFT){
-            ImageIcon tempIcon = (ImageIcon) this.tiles[row][0].getIcon();
+            ImageIcon tempIcon = (ImageIcon) this._tiles[row][0].getIcon();
             for(int i=0; i<7; i++){
                 if(i==6) {
-                    tiles[row][i].setIcon(this._extraTile.getIcon());
+                    _tiles[row][i].setIcon(this._extraTile.getIcon());
                 }
                 else{
-                    tiles[row][i].setIcon(tiles[row][i+1].getIcon());
+                    _tiles[row][i].setIcon(_tiles[row][i+1].getIcon());
                 }
                 this._extraTile.setIcon(tempIcon);
             }
@@ -119,25 +119,25 @@ public class GameUI extends JFrame {
 
     private void moveColumn(int column, Direction direction) throws Exception {
         if(direction == Direction.DOWN){
-            ImageIcon tempIcon = (ImageIcon) this.tiles[6][column].getIcon();
+            ImageIcon tempIcon = (ImageIcon) this._tiles[6][column].getIcon();
             for (int i =6; i>=0; i--){
                 if(i==0) {
-                    tiles[i][column].setIcon(this._extraTile.getIcon());
+                    _tiles[i][column].setIcon(this._extraTile.getIcon());
                 }
                 else{
-                    tiles[i][column].setIcon(tiles[i-1][column].getIcon());
+                    _tiles[i][column].setIcon(_tiles[i-1][column].getIcon());
                 }
                 this._extraTile.setIcon(tempIcon);
             }
         }
         else if(direction == Direction.UP){
-            ImageIcon tempIcon = (ImageIcon) this.tiles[0][column].getIcon();
+            ImageIcon tempIcon = (ImageIcon) this._tiles[0][column].getIcon();
             for(int i=0; i<7; i++){
                 if(i==6) {
-                    tiles[i][column].setIcon(this._extraTile.getIcon());
+                    _tiles[i][column].setIcon(this._extraTile.getIcon());
                 }
                 else{
-                    tiles[i][column].setIcon(tiles[i+1][column].getIcon());
+                    _tiles[i][column].setIcon(_tiles[i+1][column].getIcon());
                 }
                 this._extraTile.setIcon(tempIcon);
             }
@@ -147,21 +147,21 @@ public class GameUI extends JFrame {
         }
     }
 
-    public void replicateBoard(Tile[][] board, Tile extratile) throws IOException {
+    public void replicateBoard(Tile[][] board, Tile extraTile) throws IOException {
         int i = 0;
         int j = 0;
         for(Tile[] row : board){
             for(Tile tile : row){
-                tileToLabel(tile, tiles[i][j]);
+                tileToLabel(tile, _tiles[i][j]);
                 j++;
             }
             j=0;
             i++;
         }
-        tileToLabel(extratile, this._extraTile);
+        tileToLabel(extraTile, this._extraTile);
     }
 
-    public void tileToLabel(Tile tile, JLabel label) throws IOException {
+    private void tileToLabel(Tile tile, JLabel label) throws IOException {
         switch(tile.type){
             case "T":
                 if(tile.goal != null){
